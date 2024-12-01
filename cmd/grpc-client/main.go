ac8642e007d770edd25605fda54a85bf373ab4d7
Nikita Brusnikin <134952903+Mobo140@users.noreply.github.com>
2024-12-01 13:44:23 +0300

    modified grpc-client for auth

--- cmd/grpc-client/main.go
+++ cmd/grpc-client/main.go
@@ -7,33 +7,30 @@ import (
 	"log"
 
 	descAccess "github.com/Mobo140/auth/pkg/access_v1"
-	"github.com/Mobo140/microservices/chat/internal/config/env"
 	"github.com/Mobo140/microservices/chat/internal/model"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
 )
 
-var accessToken = flag.String("a", "", "access token")
+var (
+	addressAuth = "localhost:8080"
+	accessToken = flag.String("a", "", "access token")
+)
 
 func main() {
 	flag.Parse()
 
 	ctx := context.Background()
-	md := metadata.New(map[string]string{"Authorization": "Bearer" + *accessToken})
+	md := metadata.New(map[string]string{"Authorization": "Bearer " + *accessToken})
 	ctx = metadata.NewOutgoingContext(ctx, md)
 
-	creds, err := credentials.NewClientTLSFromFile("auth.pem", "")
+	creds, err := credentials.NewClientTLSFromFile("../../auth.pem", "")
 	if err != nil {
 		log.Fatalf("could not process the credentials: %v", err)
 	}
 
-	cfg, err := env.NewAuthGRPCConfig()
-	if err != nil {
-		log.Fatalf("failed to load auth grpc config: %v", err)
-	}
-
-	conn, err := grpc.NewClient(cfg.Address(), grpc.WithTransportCredentials(creds))
+	conn, err := grpc.NewClient(addressAuth, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("failed to connect to server: %v", err)
 	}
