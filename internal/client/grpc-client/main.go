9fb462a405a079c6cddde0b19c42aa60d0659be3
Nikita Brusnikin <134952903+Mobo140@users.noreply.github.com>
2024-12-06 19:35:11 +0300

    moved auth client to internal/client

---
+++ internal/client/grpc-client/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	descAccess "github.com/Mobo140/auth/pkg/access_v1"
+	"github.com/Mobo140/microservices/chat/internal/model"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+)
+
+var (
+	addressAuth = "localhost:8080"
+	accessToken = flag.String("a", "", "access token")
+)
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+	md := metadata.New(map[string]string{"Authorization": "Bearer " + *accessToken})
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	creds, err := credentials.NewClientTLSFromFile("../../auth.pem", "")
+	if err != nil {
+		log.Fatalf("could not process the credentials: %v", err)
+	}
+
+	conn, err := grpc.NewClient(addressAuth, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+
+	cl := descAccess.NewAccessV1Client(conn)
+
+	_, err = cl.Check(ctx, &descAccess.CheckRequest{
+		EndpointAddress: model.EndpointPath,
+	})
+	if err != nil {
+		log.Fatalf(err.Error()) //nolint:govet // it's ok
+	}
+
+	fmt.Println("Access granted")
+}
